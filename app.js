935db16b9a9d20676c71020ea23f8bb621c220ed
mlag1222 <lagnese.matt@gmail.com>
2016-08-10 20:14:02 -0400

    second

---
+++ app.js
@@ -0,0 +1,29 @@
+var audio; 
+
+function controlMusic(clip){
+   audio.pause()
+   audio = new Audio(clip)
+   audio.play()
+   
+};
+
+$("#heat").click(function(){
+	$(".quote").html('Brother, you are going down.');
+	controlMusic("sounds/heat.mp3");
+});
+
+$("#godfather").click(function(){
+	$(".quote").html('You broke my heart.');
+	controlMusic("sounds/brokemyheart.mp3");
+});
+	
+$("#scarface").click(function(){
+	$(".quote").html('Say hello to my leetle fren!');
+	controlMusic("sounds/1470869502.mp3");
+});
+
+$("#scent").click(function(){
+    $(".quote").html('I&#8217m just getting warmed up!');
+  	controlMusic("sounds/Scent3.mp3");
+});
+    
